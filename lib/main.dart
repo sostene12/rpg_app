76d51445cd5082550588f565df966abdf3e525ac
Ngarukiyimana Sostene <ngarukiyimanasostene@gmail.com>
2025-07-18 19:39:23 +0200

    created customized app theme

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/screens/home/home.dart';
+import 'package:flutter_rpg/theme.dart';
+
+void main() {
+  runApp(MaterialApp(theme: appTheme, home: Home()));
+}
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sandbox"),
+        backgroundColor: Colors.grey,
+      ),
+      body: const Text("Welcome to the Sandbox!"),
+    );
+  }
+}
+
+// START FROM TURTORIAL: 20
